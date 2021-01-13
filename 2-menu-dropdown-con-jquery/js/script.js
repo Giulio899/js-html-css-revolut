4cac91cf05af2b0fed32d5556ed0436b91268184
Giulio <giulio.tavoni@gmail.com>
2021-01-13 16:02:58 +0100

    Miglioramento e semplificazione del codice jQuery usando this

--- 2-menu-dropdown-con-jquery/js/script.js
+++ 2-menu-dropdown-con-jquery/js/script.js
@@ -1,21 +1,38 @@
 $(document).ready(function() {
 
-  $('#sottomenu1').hover(function() {
-    $('#sottomenu1 ul').removeClass('hidden');
+  $('.navbar-right ul li').hover(function() {
+    $(this).children('ul').removeClass('hidden');
   }, function() {
-    $('#sottomenu1 ul').addClass('hidden');
-  });
-
-  $('#sottomenu2').hover(function() {
-    $('#sottomenu2 ul').removeClass('hidden');
-  }, function() {
-    $('.navbar-right ul li ul').addClass('hidden');
-  });
-
-  $('#sottomenu3').hover(function() {
-    $('#sottomenu3 ul').removeClass('hidden');
-  }, function() {
-    $('.navbar-right ul li ul').addClass('hidden');
+    $(this).children('ul').addClass('hidden');
   });
 
 });
+
+
+
+
+
+// METODO PIU' LUNGO QUINDI MENO EFFICIENTE (avevo dato delle classi id a ciascun li)
+
+
+// $(document).ready(function() {
+//
+//   $('#sottomenu1').hover(function() {
+//     $('#sottomenu1 ul').removeClass('hidden');
+//   }, function() {
+//     $('#sottomenu1 ul').addClass('hidden');
+//   });
+//
+//   $('#sottomenu2').hover(function() {
+//     $('#sottomenu2 ul').removeClass('hidden');
+//   }, function() {
+//     $('.navbar-right ul li ul').addClass('hidden');
+//   });
+//
+//   $('#sottomenu3').hover(function() {
+//     $('#sottomenu3 ul').removeClass('hidden');
+//   }, function() {
+//     $('.navbar-right ul li ul').addClass('hidden');
+//   });
+//
+// });
